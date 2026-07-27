f4acae8f6e2aba26bb5cb4ed00f3ffdf78b7acdc
Alireza Hayati <134541207+Alirezahayatimedtech@users.noreply.github.com>
2026-07-27 20:06:55 +0330

    Add CVAT integration package

---
+++ openslit/cvat/__init__.py
@@ -0,0 +1,5 @@
+"""CVAT deployment and project-initialization integration."""
+
+from .config import CvatSetupConfig, CvatTaskPlan, load_cvat_setup_config
+
+__all__ = ["CvatSetupConfig", "CvatTaskPlan", "load_cvat_setup_config"]
